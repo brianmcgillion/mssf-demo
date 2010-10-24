9907c16d6ce2fbef409fcb9a0c1a4a6efe28b258
Brian McGillion <brian.mcgillion@symbio.com>
2010-10-24 17:05:17 +0300

    Fixes: NB#197628 - Libcreds2-Qt interface has incorrect scope in API file.

--- src/credentialsmanager.cpp
+++ src/credentialsmanager.cpp
@@ -1,26 +1,24 @@
 /*
-  * Copyright (C) 2006-2010 Nokia Corporation.
-  *
-  * This file is part of devicelock
-  *
-  * Authors: Brian McGillion <brian.mcgillion@symbio.com>,
-  * 	     Denis Mingulov  <denis.mingulov@symbio.com>
-  *
-  * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the GNU General Public License
-  * version 2 as published by the Free Software Foundation.
-  *
-  * This program is distributed in the hope that it will be useful, but
-  * WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  * General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program; if not, write to the Free Software
-  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-  * 02110-1301 USA
-  *
-  */
+ * This file is part of AEGIS
+ *
+ * Copyright (C) 2009 Nokia Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * Author: Brian McGillion <brian.mcgillion@symbio.com>
+ */
 
 #include "credentialsmanager.h"
 
